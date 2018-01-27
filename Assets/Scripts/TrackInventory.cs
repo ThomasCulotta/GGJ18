71885fb52ff3319233a602f79583b8511c833091
drstattik <35858405+drstattik@users.noreply.github.com>
2018-01-27 15:57:18 -0800

    Add files via upload
    Adding in Picking up Items and tracking in inventory

---
+++ Assets/Scripts/TrackInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackInventory : MonoBehaviour {
+
+    public bool[] itemList = { false, false, false, false, false };
+    public Text itemCountText;
+    private int collectedItemIndex;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        collectedItemIndex = Array.IndexOf(itemList, false);
+        collectedItemIndex = collectedItemIndex == -1 ? 5 : collectedItemIndex;
+        itemCountText.text = $"{collectedItemIndex}/5";
+    }
+
+    public void UpdateItemList()
+    {
+        // Find the first false instance
+        itemList[collectedItemIndex] = true;
+    }
+}
