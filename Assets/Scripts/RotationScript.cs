21c4a920c387507491394308242064279ed9905f
Thomas <thomasculotta94@gmail.com>
2018-01-27 13:38:52 -0600

    added euclid test

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed = 20f;
+    
+	void Update ()
+    {
+        transform.Rotate(0f, _rotationSpeed * Time.deltaTime, 0f);
+	}
+}
