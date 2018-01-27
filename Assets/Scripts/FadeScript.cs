0ae6a907ee48e052bd967857d945c0913837530a
Thomas <thomasculotta94@gmail.com>
2018-01-27 14:05:46 -0600

    added test fade script

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FadeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.K))
+        {
+            SteamVR_Fade.Start(Color.black, 2f);
+        }
+	}
+}
