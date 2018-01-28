47b6f41a8529fdf72c9813b038e9dd0a21aa1407
pblanc5 <pblanc5@lsu.edu>
2018-01-27 21:02:05 -0600

    Add Waypoint Wait for Pulse

--- Assets/Scripts/LocationDevice.cs
+++ Assets/Scripts/LocationDevice.cs
@@ -10,7 +10,7 @@ public class LocationDevice : MonoBehaviour {
 
     public bool CanPing => _canPing;
 
-    private const float deviceRadius = 10f;
+    public float deviceRadius = 100f;
     private const int radioLayerMask = 1 << 8;
 
     private bool _canPing;
@@ -29,13 +29,21 @@ public class LocationDevice : MonoBehaviour {
         for (int i = 0; i< hitColliders.Length; i++)
         {
            Transform radioComponentTrans = hitColliders[i].transform;
-           Instantiate(waypoint, radioComponentTrans.position, Quaternion.identity);
+            float dist = Vector3.Distance(radioComponentTrans.position, transform.position);
+           StartCoroutine(WaitForPulse(dist, radioComponentTrans.position));
         }
 
         if (hitColliders.Length > 0)
         {
             StartCoroutine(Cooldown());
         }
+    }
+
+    private IEnumerator WaitForPulse(float dist, Vector3 pos)
+    {
+        Debug.Log(dist);
+        yield return new WaitForSeconds(dist / 50f);
+        Instantiate(waypoint, pos, Quaternion.identity);
     }
 
     private IEnumerator Cooldown()
