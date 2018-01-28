950e66ab35fc5ab0023ba3f6b3a1100971daddc1
drstattik <joefitzwater@gmail.com>
2018-01-27 17:49:32 -0800

    Adding ItemPick-up script, and Mesh Colliders, to Radio Tower Model Prefabs

--- Assets/Scripts/PickUpItem.cs
+++ Assets/Scripts/PickUpItem.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿//===================== Team Snowflake ========================================
 //
 // Purpose: Pick Up (and Destroy) Object
@@ -17,13 +16,9 @@ namespace Valve.VR.InteractionSystem
     {
         public TrackInventory inventory;
 
-        private VelocityEstimator velocityEstimator;
-
         //-------------------------------------------------
         void Awake()
         {
-            velocityEstimator = GetComponent<VelocityEstimator>();
-
             Rigidbody rb = GetComponent<Rigidbody>();
             rb.maxAngularVelocity = 50.0f;
         }
@@ -48,82 +43,13 @@ namespace Valve.VR.InteractionSystem
             rb.isKinematic = true;
             rb.interpolation = RigidbodyInterpolation.None;
 
-            if (hand.controller == null)
-            {
-                velocityEstimator.BeginEstimatingVelocity();
-            }
-
+            // Drop/Detach from item and remove it
             hand.DetachObject(gameObject, false);
             hand.HoverUnlock(null);
-
             rb.gameObject.SetActive(false);
 
+            // Update Inventory
             inventory.UpdateItemList();            
         }
     }
 }
-=======
-﻿//===================== Team Snowflake ========================================
-//
-// Purpose: Pick Up (and Destroy) Object
-//
-//=============================================================================
-
-using UnityEngine;
-
-namespace Valve.VR.InteractionSystem
-{
-    //-------------------------------------------------------------------------
-    [RequireComponent(typeof(Interactable))]
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(VelocityEstimator))]
-    public class PickUpItem : MonoBehaviour
-    {
-        public TrackInventory inventory;
-
-        private VelocityEstimator velocityEstimator;
-
-        //-------------------------------------------------
-        void Awake()
-        {
-            velocityEstimator = GetComponent<VelocityEstimator>();
-
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.maxAngularVelocity = 50.0f;
-        }
-
-        //-------------------------------------------------
-        private void HandHoverUpdate(Hand hand)
-        {
-            //Trigger got pressed
-            if (hand.GetStandardInteractionButtonDown())
-            {
-                hand.AttachObject(gameObject);
-                ControllerButtonHints.HideButtonHint(hand, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
-            }
-        }
-
-        //-------------------------------------------------
-        private void OnAttachedToHand(Hand hand)
-        {
-            hand.HoverLock(null);
-
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.isKinematic = true;
-            rb.interpolation = RigidbodyInterpolation.None;
-
-            if (hand.controller == null)
-            {
-                velocityEstimator.BeginEstimatingVelocity();
-            }
-
-            hand.DetachObject(gameObject, false);
-            hand.HoverUnlock(null);
-
-            rb.gameObject.SetActive(false);
-
-            inventory.UpdateItemList();            
-        }
-    }
-}
->>>>>>> 71885fb52ff3319233a602f79583b8511c833091
